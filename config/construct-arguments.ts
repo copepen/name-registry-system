1b2e123faadb73af7a46ffc347c5465786fc37d2
daiki saito <daiki.saito92@gmail.com>
2022-01-14 12:28:49 +0900

    initial commit

---
+++ config/construct-arguments.ts
@@ -0,0 +1,11 @@
+export const mockERC20Args = {
+  name: "mock Test Token",
+  symbol: "mTTK",
+  initialSupply: "100000000000000000000000000",
+};
+
+export const vanityNameRegisterArgs = {
+  lockToken: "0xFFD0923355bCdE73b696ca6365A11Eee05c2172b",
+  treasuryAddr: "0xA557572933cA2b5f644A0fAa6383D78Ec3D4E153",
+  feeMultiplier: "5000000000000000000",
+};
---
+++ config/deployed-address.ts
@@ -0,0 +1,2 @@
+export const mockERC20 = "0xFFD0923355bCdE73b696ca6365A11Eee05c2172b";
+export const vanityNameRegisterAddr = "";
